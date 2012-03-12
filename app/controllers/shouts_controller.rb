e0618a9e0f6886ffd196295e8f56374b216a5826
Jason Draper <jason@drapergeek.com>
2012-03-12 12:34:02 -0400

    added the shout show

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,6 +1,12 @@
 class ShoutsController < ApplicationController
+
+  def show
+   @shout = Shout.find(params[:id]) 
+  end
+
   def create
    @shout = current_user.shouts.create!(params[:shout]) 
    redirect_to dashboard_path, notice: "Shouted!"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Shouter::Application.routes.draw do
   resource :dashboard, only: [:show]
-  resources :shouts, only: [:create]
+  resources :shouts, only: [:show, :create]
   root to: "welcome#index"
 end
